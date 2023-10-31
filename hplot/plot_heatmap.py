93485a289185fc602a357ef453d61c2c8a14ae2c
zyh <zhang.yuh@outlook.com>
2023-10-31 21:55:39 +0800

    update

--- hplot/plot_heatmap.py
+++ hplot/plot_heatmap.py
@@ -1,11 +1,10 @@
 import os
-from typing import List, Optional, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from matplotlib import rcParams
-
+from typing import Union, List
 from hplot.config import default_cfg
 from hplot.utils import cm2inch
 
@@ -18,34 +17,51 @@ class PloterHeatmap:
         z,
         fname,
         *,
-        cmap: Union[str, Colormap] = "Blues",
-        vmin: Optional[float] = None,
-        vmax: Optional[float] = None,
-        xtick: Union[List[float], np.ndarray, None] = None,
-        ytick: Union[List[float], np.ndarray, None] = None,
+        cmap,
+        vmin,
+        vmax,
+        center=None,
+        levels=None,
+        level_colors=None,
+        xlabel=None,
+        ylabel=None,
+        xtick_labels=None,
+        ytick_labels=None,
         usetex=False,
         display=False,
         fig_size=None,
         dpi=None,
         pad=None,
+        tick_size=None,
+        tick_label_font=None,
+        legend_font_dict=None,
+        label_font_dict=None,
     ):
         self.x = x
         self.y = y
         self.z = z
         self.fname = fname
-
         self.cmap = cmap
         self.vmin = vmin
         self.vmax = vmax
-        self.xtick = xtick
-        self.ytick = ytick
+        self.center = center
+        self.levels = levels
+        self.level_colors = level_colors
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+ 
+        self.xtick_labels = xtick_labels
+        self.ytick_labels = ytick_labels
         self.usetex = usetex
 
         self.display = display
         self.fig_size = fig_size
         self.dpi = dpi
         self.pad = pad
-
+        self.tick_size = tick_size
+        self.tick_label_font = tick_label_font
+        self.legend_font_dict = legend_font_dict
+        self.label_font_dict = label_font_dict
         plt.cla()
         plt.clf()
         plt.close()
@@ -57,10 +73,6 @@ class PloterHeatmap:
     def __preprocess(self):
         rcParams.update({"mathtext.fontset": "stix"})
         assert isinstance(self.data, (dict, list, tuple))
-
-        if isinstance(self.data, dict):
-            self.data = [self.data]
-        self.num_data = len(self.data)
 
         if self.fig_size is None:
             self.fig_size = default_cfg.fig_size
@@ -84,71 +96,55 @@ class PloterHeatmap:
             rc("font", **{"family": "serif", "serif": ["Times New Roman"]})
             rc("text", usetex=True)
 
-    def _ppc_data(self, d):
-        x = d["x"]
-        y = d["y"]
-        label = d["label"]
-        if isinstance(x, np.ndarray):
-            x = [x]
-        if isinstance(y, np.ndarray):
-            y = [y]
-
-        assert len(x) == len(y)
-
-        for i in range(len(x)):
-            x[i] = x[i].reshape(-1)
-            y[i] = y[i].reshape(-1)
-            assert x[i].shape == y[i].shape
-        x = np.stack(x)
-        y = np.stack(y)
-
-        x = x.reshape(-1)
-        y = y.reshape(-1)
-        return x, y, label
-
     def plot(self):
         self.fig, self.ax = plt.subplots(figsize=cm2inch(*self.fig_size), dpi=self.dpi)
         # plot figure
-        for i, d in enumerate(self.data):
-            x, y, label = self._ppc_data(d)
-            sns.lineplot(x=x, y=y, label=label)
+        if isinstance(self.cmap, str):
+            cmap = plt.get_cmap(self.cmap)
+        self.ax = sns.heatmap(
+            self.z,
+            cmap=cmap,
+            vmin=self.vmin,
+            vmax=self.vmax,
+            center=self.center,
+        )
+        if self.center:
+            plt.contour(self.x, colors=self.level_colors, levels=self.levels)
 
-        # tick
-        plt.tick_params(labelsize=self.tick_size)
+        if self.xtick_labels:
+            pos_list_x = self._tick2pos(self.xtick_labels, self.x)
+            plt.xticks(pos_list_x, self.xtick_labels)
+        if self.ytick_labels:
+            pos_list_y = self._tick2pos(self.ytick_labels, self.y)
+            plt.yticks(pos_list_y, self.ytick_labels)
+        
+        if self.xlabel:
+            plt.xlabel(self.xlabel, self.label_font_dict)
+        if self.ylabel:
+            plt.ylabel(self.ylabel, self.label_font_dict)
+
+        plt.tick_params(labelsize=default_cfg.tick_size)
         labels = self.ax.get_xticklabels() + self.ax.get_yticklabels()
-        [label.set_fontname(self.tick_label_font) for label in labels]
-
-        # set legend
-        legend_handles, legend_labels = self.ax.get_legend_handles_labels()
-        legend_dict = dict()
-        legend_dict["handles"] = legend_handles
-        legend_dict["labels"] = self.legend if self.legend is not None else legend_labels
-        legend_dict["columnspacing"] = 0.3
-        legend_dict["loc"] = "best" if self.legend_loc is None else self.legend_loc
-        legend_dict["frameon"] = self.legend_frameon
-        legend_dict["prop"] = default_cfg.legend_font
-        if self.ncol is not None:
-            legend_dict["ncol"] = self.ncol
-
-        if self.show_legend:
-            self.ax.legend(**legend_dict)
-        else:
-            plt.legend([], [], frameon=False)
-
-        #  label
-        plt.xlabel(self.xlabel, self.label_font_dict)
+        [label.set_fontname(default_cfg.tick_label_font) for label in labels]
         plt.ylabel(self.ylabel, self.label_font_dict)
 
-        if self.xlim is not None:
-            plt.xlim(self.xlim)
-        if self.ylim is not None:
-            plt.ylim(self.ylim)
-        if self.xticks is not None and self.xtick_labels is not None:
-            plt.xticks(self.xticks, self.xtick_labels)
-        if self.yticks is not None and self.ytick_labels is not None:
-            plt.yticks(self.yticks, self.ytick_labels)
-        if self.ticklabel_style:
-            plt.ticklabel_format(style=self.ticklabel_style, axis="x", scilimits=(0, 0))
+    def _tick2pos(
+        self,
+        tick: Union[np.ndarray, List[float]],
+        anchor: Union[np.ndarray, List[float]],
+    ) -> List[int]:
+        pos = []
+
+        if isinstance(anchor, list):
+            anchor = np.array(anchor).reshape(-1)
+
+        for t in tick:
+            t = float(t)
+            dist = (anchor - t) ** 2
+            label = np.argmin(dist)
+            pos.append(int(label))
+        return pos
+
             
     def save(self):
         if self.fname is None:
@@ -169,84 +165,47 @@ class PloterHeatmap:
         plt.close()
 
 
-def plot_sns(
-    data,
+def plot_heatmap(
+    x,
+    y,
+    z,
     fname,
     *,
+    cmap,
+    vmin,
+    vmax,
+    center=None,
+    levels=None,
+    level_colors=None,
     xlabel=None,
     ylabel=None,
-    legend=None,
-    legend_loc="best",
-    legend_frameon=False,
-    xlim=None,
-    ylim=None,
-    xticks=None,
-    yticks=None,
     xtick_labels=None,
     ytick_labels=None,
     display=False,
     fig_size=None,
     dpi=None,
-    style="white",
-    ticklabel_style=None,
-    show_legend=True,
     **kwargs,
 ):
-    """
-    plot a curve using seaborn
-    :param data: data: list[dict]:
-        data used to plot figures,
-        example: [dict(x=x1_list, y=y1_list, label=label1), dict(x=x2_list, y=y2_list, label=label2)]
-        when there is no repeat experiment data, xi_list=x, shape of x is [d,],
-                                                 yi_list=y, shape of y is [d,],
-                                                 lableli str,
-        when there is repeat experiment data, xi_list=[x1, x2, ..., xd], shape of xi is [d,],
-                                              yi_list=[y1, y2, ..., yd], shape of y is [d,],
-                                              lableli str,
 
-    :param fname: fname: str,
-        the figure will be saved here,
-        example: "./path_to_file/figure.png"
-    :param xlabel:  str
-    :param ylabel:  str
-    :param legend:
-    :param legend_loc: "best", "upper right", "upper left", "lower left", "lower right", "right", "center left",
-        "center right", "lower center", "upper center", "center"
-    :param legend_frameon: whether legend has background frame
-    :param xlim:
-    :param ylim:
-    :param xticks:
-    :param yticks:
-    :param xtick_labels:
-    :param ytick_labels:
-    :param display:
-    :param fig_size:
-    :param dpi:
-    :param kwargs:
-    :style: "white", "dark", "whitegrid", "darkgrid"
-    :ticklabel_style: "sci" or "plain"
-    :return:
-    """
-    ploter = PloterSns(
-        data,
+    ploter = PloterHeatmap(
+        x,
+        y,
+        z,
         fname,
         xlabel=xlabel,
         ylabel=ylabel,
-        legend=legend,
-        legend_loc=legend_loc,
-        legend_frameon=legend_frameon,
-        xlim=xlim,
-        ylim=ylim,
-        xticks=xticks,
-        yticks=yticks,
+        cmap=cmap,
+        vmin=vmin,
+        vmax=vmax,
+        center=center,
+        levels=levels,
+        level_colors=level_colors,
         xtick_labels=xtick_labels,
         ytick_labels=ytick_labels,
         display=display,
         fig_size=fig_size,
         dpi=dpi,
-        style=style,
-        ticklabel_style=ticklabel_style,
-        show_legend=show_legend,
+
         **kwargs,
     )
     ploter.plot()
