80168edbd6df7314cda58f8cde52d3711ba0e277
zyh <zhang.yuh@outlook.com>
2023-11-13 21:47:28 +0800

    update

---
+++ test/test_plot_heatmap.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pytest
+
+from hplot import plot_heatmap
+
+
+def test_plot_sns():
+    x = np.linspace(-10, 10, 100)
+    y = np.linspace(-10, 10, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = X ** 2 + Y ** 2
+    plot_heatmap(
+        x=x,
+        y=y,
+        z=Z,
+        fname="figure/plot_heatmap_example.png",
+        display=False,
+        cmap="BrBG",
+        vmin=0,
+        vmax=200,
+        center=None,
+        levels=[10],
+        level_colors=["red"],
+    )
+if __name__ == "__main__":
+    test_plot_sns()
