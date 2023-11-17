741bc11f096ae064036bf4ab64301cb46bf6309d
zyh <zhang.yuh@outlook.com>
2023-11-17 15:47:06 +0800

    update

--- hplot/__init__.py
+++ hplot/__init__.py
@@ -3,3 +3,4 @@ from hplot.plot_sns import plot_sns
 from hplot.plot_box import plot_box
 from hplot.plot_heatmap import plot_heatmap
 from hplot.plot_legend import plot_legend
+from hplot.plot_2y import plot_2y
--- hplot/plot_2y.py
+++ hplot/plot_2y.py
@@ -1,13 +1,16 @@
-from itertools import cycle
+import os
 from typing import Dict, List, Optional
-
-import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 from matplotlib import rcParams
+from itertools import cycle
+import matplotlib.colors as mcolors
 
-from hplot.base import Base
 from hplot.config import hConfig
 from hplot.utils import cm2inch
+from hplot.base import Base
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 
 class plot_2y(Base):
@@ -26,28 +29,42 @@ class plot_2y(Base):
         """
         :param data: list[dict]:
             data used to plot figures,
-            example: [dict(x=x1, y=y1), dict(x=x2, y=y2)]
-        :param fname:str,
+            example: [dict(x=x1_list, y=y1_list, label=label1), dict(x=x2_list, y=y2_list, label=label2)]
+            when there is no repeat experiment data, xi_list=x, shape of x is [d,],
+                                                    yi_list=y, shape of y is [d,],
+                                                    lableli str,
+            when there is repeat experiment data, xi_list=[x1, x2, ..., xd], shape of xi is [d,],
+                                                yi_list=[y1, y2, ..., yd], shape of y is [d,],
+                                                lableli str,
+        :param fname: fname: str,
             the figure will be saved here,
             example: "./path_to_file/figure.png"
-        :param xlabel: str
-        :param ylabel: str
+        :param xlabel:  str
+        :param ylabel:  str
         :param legend: list[str]
         :param display: bool
-        :param kwargs["color_list"]:
+        :param kwargs["style"]: str
+            "white", "dark", "whitegrid", "darkgrid"
+        :param kwargs["linewidths"]: List[str]
+        :param kwargs["linestyles"]: List[str]
         :param kwargs["legend_loc"]: str
             "best", "upper right", "upper left", "lower left", "lower right", "right", "center left",
             "center right", "lower center", "upper center", "center"
+        :param kwargs["legend_frameon"]: bool
         :param kwargs["legend_ncol"]: int
             number of columns in legend
+        :param kwargs["show_legend"]: bool
         :param kwargs["xlim"]:
         :param kwargs["ylim"]:
         :param kwargs["xticks"]:
         :param kwargs["yticks"]:
         :param kwargs["xtick_labels"]:
         :param kwargs["ytick_labels"]:
-        :param kwargs["linewidths"]: List[str]
-        :param kwargs["linestyles"]: List[str]
+        :param kwargs["log_yaxis"]: bool
+        :param kwargs["ticklabel_style"]: "sci" or "plain"
+        :param kwargs["ticklabel_style_axis"]: "x" or "y" or "both"
+        :param kwargs["log_yaxis"]: bool
+        :param kwargs["sub_axis"]: List of dict keys: width, height, loc, borderpad, xlim, ylim, links. link [("ul", "ll"), ("ur", "lr")]
 
         """
         super().__init__(**kwargs)
@@ -65,6 +82,7 @@ class plot_2y(Base):
         plt.cla()
         plt.clf()
         plt.close()
+
         self.__preprocess()
 
         self.run()
@@ -76,7 +94,7 @@ class plot_2y(Base):
         if isinstance(self.data, dict):
             self.data = [self.data]
         self.num_data = len(self.data)
-
+        assert self.num_data == 2, "only support 2 data"
         # use tex to render fonts, tex install required
         if hConfig.usetex:
             from matplotlib import rc
@@ -92,10 +110,33 @@ class plot_2y(Base):
                 next(tableau_colors) for _ in range(self.num_data)
             ]
 
+    def _ppc_data(self, d):
+        x = d["x"]
+        y = d["y"]
+        label = d["label"]
+        if isinstance(x, np.ndarray):
+            x = [x]
+        if isinstance(y, np.ndarray):
+            y = [y]
+
+        assert len(x) == len(y)
+
+        for i in range(len(x)):
+            x[i] = x[i].reshape(-1)
+            y[i] = y[i].reshape(-1)
+            assert x[i].shape == y[i].shape
+        x = np.stack(x)
+        y = np.stack(y)
+
+        x = x.reshape(-1)
+        y = y.reshape(-1)
+        return x, y, label
+
     def plot(self):
         self.fig, self.ax = plt.subplots(
             figsize=cm2inch(*hConfig.fig_size), dpi=hConfig.dpi
         )
+        # plot figure
         lws = self._kwargs.get("linewidths", None)
         if lws is not None:
             assert len(self.data) == len(lws)
@@ -104,44 +145,88 @@ class plot_2y(Base):
         if lss is not None:
             assert len(self.data) == len(lss)
 
-        assert len(self.data) == 2  # only support 2 data
-        # plot figure
         cl = self._kwargs.get("color_list", None)
         assert cl is not None
 
-        i = 1
-        d = self.data[0]
-        lw = lws[i] if lws is not None else 2
-        ls = lss[i] if lss is not None else "-"
-        self.ax.plot(d["x"], d["y"], color=cl[i], linewidth=lw, ls=ls)
+        handles = []
+        style = self._kwargs.get("style", "white")
+        with sns.axes_style(style):
+            i = 0
+            d = self.data[i]
+            x, y, label = self._ppc_data(d)
+            lw = lws[i] if lws is not None else 2
+            ls = lss[i] if lss is not None else "-"
+            color = cl[i]
+            l1 = sns.lineplot(
+                x=x,
+                y=y,
+                label=label,
+                c=color,
+                linewidth=lw,
+                ls=ls,
+                ax=self.ax,
+                legend=False,
+            )
+            handles.append(l1)
+        with sns.axes_style("white"):
+            i = 1
+            d = self.data[i]
+            x, y, label = self._ppc_data(d)
+            lw = lws[i] if lws is not None else 2
+            ls = lss[i] if lss is not None else "-"
+            color = cl[i]
+            ax2 = self.ax.twinx()
+            sns.lineplot(
+                x=x,
+                y=y,
+                label=label,
+                c=color,
+                linewidth=lw,
+                ls=ls,
+                legend=False,
+                ax=ax2,
+            )
 
-        self.ax2 = self.ax.twinx()
-        i = 2
-        d = self.data[1]
-        lw = lws[i] if lws is not None else 2
-        ls = lss[i] if lss is not None else "-"
-        self.ax2.plot(d["x"], d["y"], color=cl[i], linewidth=lw, ls=ls)
-
-        # legend
+        # tick
         plt.tick_params(labelsize=hConfig.tick_size)
         labels = self.ax.get_xticklabels() + self.ax.get_yticklabels()
         [label.set_fontname(hConfig.tick_label_font) for label in labels]
+        labels = ax2.get_xticklabels() + ax2.get_yticklabels()
+        [label.set_fontname(hConfig.tick_label_font) for label in labels]
 
-        if self.legend is not None:
-            plt.legend(
-                self.legend,
-                loc=self._kwargs.get("legend_loc", "best"),
-                ncol=self._kwargs.get("legend_ncol", 1),
-                prop=hConfig.legend_font,
-            )
+        # set legend
+        legend_handles, legend_labels = self.ax.get_legend_handles_labels()
+        legend_handles2, legend_labels2 = ax2.get_legend_handles_labels()
+        legend_handles.extend(legend_handles2)
+        legend_labels.extend(legend_labels2)
+        legend_dict = dict()
+        legend_dict["handles"] = legend_handles
+        legend_dict["labels"] = (
+            self.legend if self.legend is not None else legend_labels
+        )
+        legend_dict["columnspacing"] = 0.3
+        legend_loc = self._kwargs.get("legend_loc", "best")
+        legend_dict["loc"] = "best" if legend_loc is None else legend_loc
+        legend_dict["frameon"] = self._kwargs.get("legend_frameon", True)
+        legend_dict["prop"] = hConfig.legend_font
+
+        ncol = self._kwargs.get("legend_ncol", 1)
+        if ncol is not None:
+            legend_dict["ncol"] = ncol
+
+        if self._kwargs.get("show_legend", True):
+            self.ax.legend(**legend_dict)
+        else:
+            plt.legend([], [], frameon=False)
 
         #  label
         if self.xlabel is not None:
-            plt.xlabel(self.xlabel, hConfig.label_font)
-        if self.ylabel is not None:
-            plt.ylabel(self.ylabel, hConfig.label_font)
+            self.ax.set_xlabel(self.xlabel, hConfig.label_font)
+        if self.xlabel is not None:
+            self.ax.set_ylabel(self.ylabel, hConfig.label_font)
         if self.y2label is not None:
-            self.ax2.set_ylabel(self.y2label, hConfig.label_font)
+            ax2.set_ylabel(self.y2label, hConfig.label_font)
+
         xlim = self._kwargs.get("xlim", None)
         if xlim is not None:
             plt.xlim(xlim)
@@ -158,3 +243,14 @@ class plot_2y(Base):
             plt.xticks(xticks, xtick_labels)
         if yticks is not None and ytick_labels is not None:
             plt.yticks(yticks, ytick_labels)
+
+        ticklabel_style = self._kwargs.get("ticklabel_style", None)
+        ticklabel_style_axis = self._kwargs.get("ticklabel_style_axis", "x")
+        if ticklabel_style:
+            plt.ticklabel_format(
+                style=ticklabel_style, axis=ticklabel_style_axis, scilimits=(0, 0)
+            )
+
+        log_yaxis = self._kwargs.get("log_yaxis", None)
+        if log_yaxis is not None:
+            plt.yscale("log")
